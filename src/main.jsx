f463d7a374dd0350e9a1ac640985a0b36a381a9a
miracsengonul <miracsengonul@gmail.com>
2023-11-06 18:04:37 +0100

    responsive and vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./assets/global.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./route/routes.jsx";
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
   </React.StrictMode>,
 );
--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -4,11 +4,11 @@ import { Tooltip } from "react-tooltip";
 
 function MainLayout() {
   return (
-    <div className="w-full min-h-screen bg-gray-50 mx-auto min-w-max">
+    <div className="w-full min-h-screen bg-gray-50 lg:mx-auto lg:min-w-max">
       <div className="border-b p-2 flex items-center justify-around dark:bg-gray-950 dark:border-gray-500">
         <Link to='/' className="flex items-center gap-x-2 select-none">
           <img src="/logo.png" alt="" width="64" />
-          <span className="text-2xl font-bold bg-gradient-to-r from-orange-400 to-blue-400 bg-clip-text text-transparent">
+          <span className="hidden lg:block lg:text-2xl font-bold bg-gradient-to-r from-orange-400 to-blue-400 bg-clip-text text-transparent">
             Public MRR
           </span>
         </Link>
@@ -18,11 +18,11 @@ function MainLayout() {
             rel="noreferrer"
           data-tooltip-content="MRR must be at least $1"
           data-tooltip-id="tooltip"
-          className="border p-2 rounded-md transition-all border-b-2 active:border cursor-pointer select-none dark:text-gray-300 dark:border-gray-500"
+          className="border p-2 rounded-md transition-all text-sm lg:text-base border-b-2 active:border cursor-pointer select-none dark:text-gray-300 dark:border-gray-500"
         >
-          Submit your profile
+          Share your profile
         </Link>
-        <Link to='https://mirac.lemonsqueezy.com/checkout/buy/584ed4e9-1325-4b28-962a-14e426535e7a' target='_blank' rel='noreferrer'>
+        <Link className='text-sm lg:text-base' to='https://mirac.lemonsqueezy.com/checkout/buy/584ed4e9-1325-4b28-962a-14e426535e7a' target='_blank' rel='noreferrer'>
           Buy Pre-Order
         </Link>
         <Tooltip id="tooltip" />
